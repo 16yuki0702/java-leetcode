2fdea3f238220e4896b30a53616cb73140374040
16yuki0702 <hs19870702@gmail.com>
2023-10-27 18:12:51 +0900

    Add cases

--- main.java
+++ main.java
@@ -848,3 +848,50 @@ class Solution {
         return dummy.next;
     }
 }
+
+// 25. Reverse Nodes in k-Group
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode reverse(ListNode begin, ListNode end) {
+        ListNode curr = begin.next;
+        ListNode next;
+        ListNode prev = begin;
+        ListNode first = curr;
+        while (curr != end) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        begin.next = prev;
+        first.next = curr;
+        return first;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null || k == 1) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1, head);
+        ListNode begin = dummy;
+        int i = 0;
+        while (head != null) {
+            i++;
+            if (i % k == 0) {
+                begin = reverse(begin, head.next);
+                head = begin.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+}
