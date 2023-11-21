4c180209692e13567df903f8c642b55f71f578ef
16yuki0702 <hs19870702@gmail.com>
2023-11-21 10:02:46 +0900

    Add cases

--- main.java
+++ main.java
@@ -923,3 +923,16 @@ class Solution {
     }
 }
 */
+
+// 26. Remove Duplicates from Sorted Array
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                nums[++i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
