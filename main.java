06fa61b2e04fe787496d707e4cdebb993caf8b82
16yuki0702 <hs19870702@gmail.com>
2023-12-04 10:38:26 +0900

    Add cases

--- main.java
+++ main.java
@@ -1068,3 +1068,25 @@ class Solution {
         }
     }
 }
+
+// 32. Longest Valid Parentheses
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    res = Math.max(res, i - stack.peek());
+                }
+            }
+        }
+        return res;
+    }
+}
