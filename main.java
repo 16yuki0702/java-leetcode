d534cee7204d3db9b8896f2b08b20ebf88fa4d99
16yuki0702 <hs19870702@gmail.com>
2024-06-04 09:18:50 +0900

    Add cases

--- main.java
+++ main.java
@@ -3825,3 +3825,39 @@ class Solution {
         return res;
     }
 }
+
+// 129. Sum Root to Leaf Numbers
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int sumNumbers(TreeNode root) {
+        recursive(root, 0);
+        return res;
+    }
+    private void recursive(TreeNode root, int curr) {
+        if (root == null) {
+            return;
+        }
+        curr = curr * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            res += curr;
+            return;
+        }
+        recursive(root.left, curr);
+        recursive(root.right, curr);
+    }
+}
