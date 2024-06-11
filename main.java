6d55f15e66dcd0277711bac1e63732ae13d16b5c
16yuki0702 <hs19870702@gmail.com>
2024-06-11 09:35:58 +0900

    Add cases

--- main.java
+++ main.java
@@ -3960,3 +3960,42 @@ class Solution {
         return true;
     }
 }
+
+// 133. Clone Graph
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Integer, Node> m = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return node;
+        }
+        if (m.containsKey(node.val)) {
+            return m.get(node.val);
+        }
+        Node n = new Node(node.val);
+        m.put(n.val, n);
+        for (Node child : node.neighbors) {
+            n.neighbors.add(cloneGraph(child));
+        }
+        return n;
+    }
+}
