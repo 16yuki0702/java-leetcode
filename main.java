7c9fb3082c03b83ae12621978e967b9794555eab
16yuki0702 <hs19870702@gmail.com>
2024-06-19 10:50:50 +0900

    Add cases

--- main.java
+++ main.java
@@ -4042,3 +4042,14 @@ class Solution {
         return res;
     }
 }
+
+// 136. Single Number
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int n : nums) {
+            res ^= n;
+        }
+        return res;
+    }
+}
