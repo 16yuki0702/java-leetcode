79bcd703bcc1b21e5cac0be3a8ce0c2d4eaa6796
16yuki0702 <hs19870702@gmail.com>
2024-07-09 10:56:07 +0900

    Add cases

--- main.java
+++ main.java
@@ -4211,3 +4211,98 @@ public class Solution {
         return null;
     }
 }
+
+// 143. Reorder List
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode curr = head;
+        Stack<ListNode> stack = new Stack<>();
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.next;
+        }
+        int count = stack.size() / 2;
+        while (count-- != 0) {
+            ListNode node = stack.pop();
+            ListNode next = head.next;
+            head.next = node;
+            node.next = next;
+            head = next;
+        }
+        head.next = null;
+    }
+}
+
+// 144. Binary Tree Preorder Traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> res = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        preorder(root);
+        return res;
+    }
+    private void preorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
+
+// 145. Binary Tree Postorder Traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> res = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postorder(root);
+        return res;
+    }
+    private void postorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        res.add(root.val);
+    }
+}
