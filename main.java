f18318f4d9aa81e0dbcf9412fc943b65e07e2f6a
16yuki0702 <hs19870702@gmail.com>
2024-10-09 15:38:08 +0900

    Add cases

--- main.java
+++ main.java
@@ -4790,3 +4790,20 @@ class Solution {
         return res.toString();
     }
 }
+
+// 167. Two Sum II - Input Array Is Sorted
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        while (l < r) {
+            if (numbers[l] + numbers[r] == target) {
+                return new int[]{l + 1, r + 1};
+            } else if (numbers[l] + numbers[r] > target) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return new int[]{};
+    }
+}
