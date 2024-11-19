2446275f7c5da9ccb9bcbde14497db3b40554d97
16yuki0702 <hs19870702@gmail.com>
2024-11-19 17:49:57 +0900

    Add cases

--- main.java
+++ main.java
@@ -4945,3 +4945,17 @@ class Solution {
         return res.toString();
     }
 }
+
+// 187. Repeated DNA Sequences
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> seen = new HashSet<>(), repeated = new HashSet<>();
+        for (int i = 0; i + 9 < s.length(); i++) {
+            String ten = s.substring(i, i + 10);
+            if (!seen.add(ten)) {
+                repeated.add(ten);
+            }
+        }
+        return new ArrayList<>(repeated);
+    }
+}
