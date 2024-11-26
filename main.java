85f9a61c221a0c24f10a36bffa3c7ac057ce7f9a
16yuki0702 <hs19870702@gmail.com>
2024-11-26 10:40:51 +0900

    Add cases

--- main.java
+++ main.java
@@ -4980,3 +4980,22 @@ class Solution {
         return dp[0][0][k];
     }
 }
+
+// 189. Rotate Array
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
