34038b02ec52a46579131a66d527303a961d1b90
16yuki0702 <hs19870702@gmail.com>
2024-11-27 11:04:37 +0900

    Add cases

--- main.java
+++ main.java
@@ -4999,3 +4999,17 @@ class Solution {
         }
     }
 }
+
+// 190. Reverse Bits
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= n & 1;
+            n >>= 1;
+        }
+        return res;
+    }
+}
