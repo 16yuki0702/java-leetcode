e46cbe0d3cf89e79a21fd16212a5bf71cebba9e3
16yuki0702 <hs19870702@gmail.com>
2024-12-02 13:28:44 +0900

    Add cases

--- main.java
+++ main.java
@@ -5041,3 +5041,37 @@ class Solution {
         return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
     }
 }
+
+// 199. Binary Tree Right Side View
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> res = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        dfs(root, 1);
+        return res;
+    }
+    private void dfs(TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+        if (res.size() < level) {
+            res.add(root.val);
+        }
+        dfs(root.right, level + 1);
+        dfs(root.left, level + 1);
+    }
+}
