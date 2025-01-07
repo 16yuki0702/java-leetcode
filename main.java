1eea0aeafde10cb706c8fa20431f740600bbf054
16yuki0702 <hs19870702@gmail.com>
2025-01-07 17:58:08 +0900

    Add cases

--- main.java
+++ main.java
@@ -5075,3 +5075,32 @@ class Solution {
         dfs(root.left, level + 1);
     }
 }
+
+// 200. Number of Islands
+class Solution {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || grid.length <= i || j < 0 || grid[0].length <= j) {
+            return;
+        }
+        if (grid[i][j] == '2' || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '2';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
