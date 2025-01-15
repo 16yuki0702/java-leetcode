5a2ac72497cdd529456ccc5d4f2e8db955e32b5e
16yuki0702 <hs19870702@gmail.com>
2025-01-15 17:32:10 +0900

    Add cases

--- main.java
+++ main.java
@@ -5136,3 +5136,31 @@ class Solution {
         }
     }
 }
+
+// 203. Remove Linked List Elements
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode fake = new ListNode(-1, head);
+        ListNode curr = fake, prev = fake;
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+                curr = curr.next;
+                continue;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+        return fake.next;
+    }
+}
