d11f33126ece5fa0155fe9cf1c6d0df1b792be41
16yuki0702 <hs19870702@gmail.com>
2025-01-27 09:46:18 +0900

    Add cases

--- main.java
+++ main.java
@@ -5190,3 +5190,22 @@ class Solution {
         return count;
     }
 }
+
+// 205. Isomorphic Strings
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> charMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i), tc = t.charAt(i);
+            if (charMap.containsKey(sc)) {
+                if (charMap.get(sc) != tc) {
+                    return false;
+                }
+            } else if (charMap.containsValue(tc)) {
+                return false;
+            }
+            charMap.put(sc, tc);
+        }
+        return true;
+    }
+}
