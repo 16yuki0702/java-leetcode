6332df334625c01b28d93983cd15ab917413e745
16yuki0702 <hs19870702@gmail.com>
2025-02-04 09:14:56 +0900

    Add cases

--- main.java
+++ main.java
@@ -5209,3 +5209,27 @@ class Solution {
         return true;
     }
 }
+
+// 206. Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
