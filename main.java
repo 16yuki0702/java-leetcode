c57cad2b611fb4e6c099bef641272a21ecbcf01f
16yuki0702 <hs19870702@gmail.com>
2025-03-27 14:40:47 +0900

    Add cases

--- main.java
+++ main.java
@@ -5505,3 +5505,25 @@ class Solution {
         board[i][j] = c;
     }
 }
+
+// 213. House Robber II
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp1 = new int[nums.length];
+        int[] dp2 = new int[nums.length];
+
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(nums[0], nums[1]);
+
+        dp2[1] = nums[1];
+
+        for (int i = 2; i < nums.length; i++) {
+            dp1[i] = Math.max(nums[i] + dp1[i - 2], dp1[i - 1]);
+            dp2[i] = Math.max(nums[i] + dp2[i - 2], dp2[i - 1]);
+        }
+        return Math.max(dp1[nums.length - 2], dp2[nums.length - 1]);
+    }
+}
