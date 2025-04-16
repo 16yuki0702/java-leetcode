5129e6c24862173e1dd3bb408ee5468d37d8982a
16yuki0702 <hs19870702@gmail.com>
2025-04-16 11:54:06 +0900

    Add cases

--- main.java
+++ main.java
@@ -5614,3 +5614,14 @@ class Solution {
         }
     }
 }
+
+// 217. Contains Duplicate
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for (int n : nums) {
+            s.add(n);
+        }
+        return s.size() < nums.length;
+    }
+}
