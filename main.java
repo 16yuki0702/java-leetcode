fa8bf41308262bc302ef856e7dea5c932561daf2
16yuki0702 <hs19870702@gmail.com>
2025-05-28 16:25:21 +0900

    Add cases

--- main.java
+++ main.java
@@ -5835,3 +5835,38 @@ class Solution {
         return area1 + area2 - overlapArea;
     }
 }
+
+// 224. Basic Calculator
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int res = 0, number = 0, sign = 1;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                number = 10 * number + (int)(c - '0');
+            } else if (c == '+') {
+                res += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (c == '-') {
+                res += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                sign = 1;
+                res = 0;
+            } else if (c == ')') {
+                res += sign * number;
+                number = 0;
+                res *= stack.pop();
+                res += stack.pop();
+            }
+        }
+        if (number != 0) {
+            res += sign * number;
+        }
+        return res;
+    }
+}
