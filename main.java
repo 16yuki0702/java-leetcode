096dc8f04259d742e15bb52a5fd28e954dce43fc
16yuki0702 <hs19870702@gmail.com>
2025-06-10 16:13:52 +0900

    Add cases

--- main.java
+++ main.java
@@ -5931,3 +5931,39 @@ class Solution {
         return root;
     }
 }
+
+// 227. Basic Calculator II
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        char operator = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            if (isOperator(c) || i == s.length() - 1) {
+                if (operator == '+') {
+                    stack.push(num);
+                } else if (operator == '-') {
+                    stack.push(-num);
+                } else if (operator == '*') {
+                    stack.push(stack.pop() * num);
+                } else if (operator == '/') {
+                    stack.push(stack.pop() / num);
+                }
+                num = 0;
+                operator = c;
+            }
+        }
+        int res = 0;
+        while (!stack.isEmpty()) {
+            res += stack.pop();
+        }
+        return res;
+    }
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+}
