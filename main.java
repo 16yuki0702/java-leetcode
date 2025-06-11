165d4383d6ec6df5b893834042cf447743a049a0
16yuki0702 <hs19870702@gmail.com>
2025-06-11 10:38:52 +0900

    Add cases

--- main.java
+++ main.java
@@ -5967,3 +5967,22 @@ class Solution {
         return c == '+' || c == '-' || c == '*' || c == '/';
     }
 }
+
+// 228. Summary Ranges
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            if (start != nums[i]) {
+                res.add("" + start + "->" + nums[i]);
+            } else {
+                res.add("" + start);
+            }
+        }
+        return res;
+    }
+}
