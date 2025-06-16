e1f97a196694ff649988090da67db543316b8cba
16yuki0702 <hs19870702@gmail.com>
2025-06-16 10:48:19 +0900

    Add cases

--- main.java
+++ main.java
@@ -5986,3 +5986,47 @@ class Solution {
         return res;
     }
 }
+
+// 229. Majority Element II
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int count1 = 0, count2 = 0;
+        int candidate1 = 0, candidate2 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count1 == 0 && nums[i] != candidate2) {
+                count1 = 1;
+                candidate1 = nums[i];
+            } else if (count2 == 0 && nums[i] != candidate1) {
+                count2 = 1;
+                candidate2 = nums[i];
+            } else if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int threshold = nums.length / 3;
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            }
+        }
+        if (count1 > threshold) {
+            res.add(candidate1);
+        }
+        if (count2 > threshold) {
+            res.add(candidate2);
+        }
+        return res;
+    }
+}
