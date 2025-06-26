dd8b2e51852a6ec4ae7511e8f30208d72c273531
16yuki0702 <hs19870702@gmail.com>
2025-06-26 09:19:19 +0900

    Add cases

--- main.java
+++ main.java
@@ -6120,3 +6120,14 @@ class MyQueue {
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
  */
+
+// 233. Number of Digit One
+class Solution {
+    public int countDigitOne(int n) {
+        int ones = 0;
+        for (long m = 1; m <= n; m *= 10) {
+            ones += (n / m + 8) / 10 * m + (n / m % 10 == 1 ? n % m + 1 : 0);
+        }
+        return ones;
+    }
+}
