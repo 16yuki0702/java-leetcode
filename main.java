66dcf4fb80ff4de7ce4a82663862a48f580ac3a7
16yuki0702 <hs19870702@gmail.com>
2025-07-10 14:53:51 +0900

    Add cases

--- main.java
+++ main.java
@@ -6238,3 +6238,23 @@ class Solution {
         node.next = node.next.next;
     }
 }
+
+// 238. Product of Array Except Self
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int res[] = new int[n];
+        Arrays.fill(res, 1);
+        int curr = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] *= curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= curr;
+            curr *= nums[i];
+        }
+        return res;
+    }
+}
