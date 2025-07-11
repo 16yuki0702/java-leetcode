bd4596e434ed9a649cee75e4cad0dbd52ef5e72a
16yuki0702 <hs19870702@gmail.com>
2025-07-12 07:57:09 +0900

    Add cases

--- main.java
+++ main.java
@@ -6284,3 +6284,24 @@ class Solution {
         return res;
     }
 }
+
+// 240. Search a 2D Matrix II
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+            return false;
+        }
+        int col = matrix[0].length - 1;
+        int row = 0;
+        while (col >= 0 && row <= matrix.length - 1) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else if (target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+}
