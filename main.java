ca44938352a740e0b7f0d968356550ae26f112b0
16yuki0702 <hs19870702@gmail.com>
2025-07-16 09:10:56 +0900

    Add cases

--- main.java
+++ main.java
@@ -6334,3 +6334,23 @@ class Solution {
         return res;
     }
 }
+
+// 242. Valid Anagram
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alpha = new int[26];
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            alpha[s.charAt(i) - 'a']++;
+            alpha[t.charAt(i) - 'a']--;
+        }
+        for (int i : alpha) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
