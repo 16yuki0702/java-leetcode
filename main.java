ad6d1c76a097e22f6512f3d5d5bddcc6a9d48a9c
16yuki0702 <hs19870702@gmail.com>
2025-07-18 08:12:53 +0900

    Add cases

--- main.java
+++ main.java
@@ -6354,3 +6354,40 @@ class Solution {
         return true;
     }
 }
+
+// 257. Binary Tree Paths
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<String> res = new ArrayList<String>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root != null) {
+            searchBST(root, "");
+        }
+        return res;
+    }
+    private void searchBST(TreeNode root, String path) {
+        if (root.left == null && root.right == null) {
+            res.add(path + root.val);
+        }
+        if (root.left != null) {
+            searchBST(root.left, path + root.val + "->");
+        }
+        if (root.right != null) {
+            searchBST(root.right, path + root.val + "->");
+        }
+    }
+}
