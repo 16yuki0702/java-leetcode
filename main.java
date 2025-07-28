183e3d06c3ef340bdb34379ed7e24b9d2416ed0f
16yuki0702 <hs19870702@gmail.com>
2025-07-28 09:04:30 +0900

    Add cases

--- main.java
+++ main.java
@@ -6404,3 +6404,21 @@ class Solution {
         }
     }
 }
+
+// 260. Single Number III
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int[] res = new int[2];
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                res[index++] = entry.getKey();
+            }
+        }
+        return res;
+    }
+}
