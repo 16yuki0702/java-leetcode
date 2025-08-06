1d1f01573ac18aadeda4479a157631fa690ed84b
16yuki0702 <hs19870702@gmail.com>
2025-08-06 17:24:56 +0900

    Add cases

--- main.java
+++ main.java
@@ -6422,3 +6422,15 @@ class Solution {
         return res;
     }
 }
+
+// 263. Ugly Number
+class Solution {
+    public boolean isUgly(int n) {
+        for (int i = 2; i < 6 && 0 < n; i++) {
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+        return n == 1;
+    }
+}
