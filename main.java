85483cdfa19f2c2e07f62611daf61a442478c0d9
16yuki0702 <hs19870702@gmail.com>
2025-08-12 11:38:19 +0900

    Add cases

--- main.java
+++ main.java
@@ -6434,3 +6434,27 @@ class Solution {
         return n == 1;
     }
 }
+
+// 264. Ugly Number II
+class Solution {
+    public int nthUglyNumber(int n) {
+        int res[] = new int[n];
+        res[0] = 1;
+        int idx2 = 0, idx3 = 0, idx5 = 0;
+        int factor2 = 2, factor3 = 3, factor5 = 5;
+        for (int i = 1; i < n; i++) {
+            int min = Math.min(Math.min(factor2, factor3), factor5);
+            res[i] = min;
+            if (factor2 == min) {
+                factor2 = 2 * res[++idx2];
+            }
+            if (factor3 == min) {
+                factor3 = 3 * res[++idx3];
+            }
+            if (factor5 == min) {
+                factor5 = 5 * res[++idx5];
+            }
+        }
+        return res[n - 1];
+    }
+}
