81d78ac97e255407fbda4e822560a8586a2969ce
16yuki0702 <hs19870702@gmail.com>
2025-09-10 14:32:43 +0900

    Add cases

--- main.java
+++ main.java
@@ -6561,3 +6561,21 @@ class Solution {
         return 0;
     }
 }
+
+// 275. H-Index II
+class Solution {
+    public int hIndex(int[] citations) {
+        int low = 0, high = citations.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (citations[mid] == citations.length - mid) {
+                return citations[mid];
+            } else if (citations[mid] < citations.length - mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return citations.length - low;
+    }
+}
