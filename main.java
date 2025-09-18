30a6067cf3c6c064be0d13a508a99ee271156a63
16yuki0702 <hs19870702@gmail.com>
2025-09-18 09:22:44 +0900

    Add cases

--- main.java
+++ main.java
@@ -6598,3 +6598,20 @@ public class Solution extends VersionControl {
         return low;
     }
 }
+
+// 279. Perfect Squares
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j] + 1);
+            }
+            dp[i] = min;
+        }
+        return dp[n];
+    }
+}
