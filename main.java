66787638d885ce5eb4a9c4991afd0adfa0b068cb
16yuki0702 <hs19870702@gmail.com>
2025-09-22 09:54:37 +0900

    Add cases

--- main.java
+++ main.java
@@ -6615,3 +6615,36 @@ class Solution {
         return dp[n];
     }
 }
+
+// 282. Expression Add Operators
+class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<String>();
+        if (num == null || num.length() == 0) {
+            return res;
+        }
+        helper(res, "", num, target, 0, 0, 0);
+        return res;
+    }
+    private void helper(List<String> res, String path, String num, int target, int pos, long eval, long multed) {
+        if (pos == num.length()) {
+            if (target == eval) {
+                res.add(path);
+            }
+            return;
+        }
+        for (int i = pos; i < num.length(); i++) {
+            if (i != pos && num.charAt(pos) == '0') {
+                break;
+            }
+            long curr = Long.parseLong(num.substring(pos, i + 1));
+            if (pos == 0) {
+                helper(res, path + curr, num, target, i + 1, curr, curr);
+            } else {
+                helper(res, path + "+" + curr, num, target, i + 1, eval + curr, curr);
+                helper(res, path + "-" + curr, num, target, i + 1, eval - curr, -curr);
+                helper(res, path + "*" + curr, num, target, i + 1, eval - multed + multed * curr, multed * curr);
+            }
+        }
+    }
+}
