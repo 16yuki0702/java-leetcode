16cd859ee4ab03f6133bd17c57db9d9bc479af6a
16yuki0702 <hs19870702@gmail.com>
2025-10-20 09:28:34 +0900

    Add cases

--- main.java
+++ main.java
@@ -6894,8 +6894,32 @@ public class Codec {
         return root;
     }
 }
-
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
 // Codec deser = new Codec();
 // TreeNode ans = deser.deserialize(ser.serialize(root));
+
+// 299. Bulls and Cows
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0, cows = 0;
+        int[] numbers = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            int s = Character.getNumericValue(secret.charAt(i));
+            int g = Character.getNumericValue(guess.charAt(i));
+            if (s == g) {
+                bulls++;
+            } else {
+                if (numbers[s] < 0) {
+                    cows++;
+                }
+                if (numbers[g] > 0) {
+                    cows++;
+                }
+                numbers[s]++;
+                numbers[g]--;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
