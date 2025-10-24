5a023da86a33448c1d6f7d4ab2cebce9ba736b70
16yuki0702 <hs19870702@gmail.com>
2025-10-24 10:42:58 +0900

    Add cases

--- main.java
+++ main.java
@@ -6923,3 +6923,33 @@ class Solution {
         return bulls + "A" + cows + "B";
     }
 }
+
+// 300. Longest Increasing Subsequence
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int n : nums) {
+            if (res.isEmpty() || res.get(res.size() - 1) < n) {
+                res.add(n);
+            } else {
+                int idx = binarySearch(res, n);
+                res.set(idx, n);
+            }
+        }
+        return res.size();
+    }
+    private int binarySearch(List<Integer> arr, int target) {
+        int left = 0, right = arr.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr.get(mid) == target) {
+                return mid;
+            } else if (arr.get(mid) > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
