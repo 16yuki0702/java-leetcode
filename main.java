ce52d3a22d077e48b96926b7a19fb3d4f27a516c
16yuki0702 <hs19870702@gmail.com>
2025-11-04 09:55:32 +0900

    Add cases

--- main.java
+++ main.java
@@ -6987,3 +6987,26 @@ class Solution {
         }
     }
 }
+
+// 303. Range Sum Query - Immutable
+class NumArray {
+    private int[] prefixSum;
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        this.prefixSum = new int[n + 1];
+        prefixSum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
