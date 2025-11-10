188c9b2212a3235e9cad02e396f2dc8483933ee5
16yuki0702 <hs19870702@gmail.com>
2025-11-10 09:03:23 +0900

    Add cases

--- main.java
+++ main.java
@@ -7010,3 +7010,27 @@ class NumArray {
  * NumArray obj = new NumArray(nums);
  * int param_1 = obj.sumRange(left,right);
  */
+
+// 304. Range Sum Query 2D - Immutable
+class NumMatrix {
+    long[][] dp;
+    public NumMatrix(int[][] matrix) {
+        int ylen = matrix.length + 1, xlen = matrix[0].length + 1;
+        dp = new long[ylen][xlen];
+        for (int i = 1; i < ylen; i++) {
+            for (int j = 1;j < xlen; j++) {
+                dp[i][j] = matrix[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return (int)(dp[row2 + 1][col2 + 1] - dp[row2 + 1][col1] - dp[row1][col2 + 1] + dp[row1][col1]);
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
