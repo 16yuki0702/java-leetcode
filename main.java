49f4219a3a67386e1590ef9548d8c28ee7d05a87
16yuki0702 <hs19870702@gmail.com>
2026-01-23 09:43:21 +0900

    Add cases

--- main.java
+++ main.java
@@ -7359,3 +7359,20 @@ class Solution {
         return res;
     }
 }
+
+// 322. Coin Change
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
