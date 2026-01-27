865fb1e131cf5a85c3c1f317a4eb7d4c41fb9220
16yuki0702 <hs19870702@gmail.com>
2026-01-27 09:21:18 +0900

    Add cases

--- main.java
+++ main.java
@@ -7376,3 +7376,18 @@ class Solution {
         return dp[amount] > amount ? -1 : dp[amount];
     }
 }
+
+// 324. Wiggle Sort II
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int n = nums.length - 1;
+        int[] arr = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(arr);
+        for (int i = 1; i < nums.length; i += 2) {
+            nums[i] = arr[n--];
+        }
+        for (int i = 0; i < nums.length; i += 2) {
+            nums[i] = arr[n--];
+        }
+    }
+}
