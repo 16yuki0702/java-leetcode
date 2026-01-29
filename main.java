25cd6d60996261487193eaea8ad07bc15c5a98bc
16yuki0702 <hs19870702@gmail.com>
2026-01-29 10:42:33 +0900

    Add cases

--- main.java
+++ main.java
@@ -7404,3 +7404,40 @@ class Solution {
         return n == 1;
     }
 }
+
+// 327. Count of Range Sum
+class Solution {
+    public int countRangeSum(int[] nums, int lower, int upper) {
+        int n = nums.length;
+        long[] sums = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+        return countWithMergeSort(sums, 0, n + 1, lower, upper);
+    }
+    private int countWithMergeSort(long[] sums, int start, int end, int lower, int upper) {
+        if (end - start <= 1) {
+            return 0;
+        }
+        int mid = start + (end - start) / 2;
+        int count = countWithMergeSort(sums, start, mid, lower, upper)
+                    + countWithMergeSort(sums, mid, end, lower, upper);
+        int j = mid, k = mid, t = mid;
+        long[] cache = new long[end - start];
+        for (int i = start, r = 0; i < mid; i++, r++) {
+            while (k < end && sums[k] - sums[i] < lower) {
+                k++;
+            }
+            while (j < end && sums[j] - sums[i] <= upper) {
+                j++;
+            }
+            while (t < end && sums[t] < sums[i]) {
+                cache[r++] = sums[t++];
+            }
+            cache[r] = sums[i];
+            count += j - k;
+        }
+        System.arraycopy(cache, 0, sums, start, t - start);
+        return count;
+    }
+}
