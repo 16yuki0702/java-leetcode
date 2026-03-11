83966cc21bf6fe7a62e09ed0f0289744e021c4bc
16yuki0702 <hs19870702@gmail.com>
2026-03-11 11:17:01 +0900

    Add cases

--- main.java
+++ main.java
@@ -7509,3 +7509,21 @@ class Solution {
         return max;
     }
 }
+
+// 330. Patching Array
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int result = 0, i = 0;
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                result++;
+            }
+        }
+        return result;
+    }
+}
