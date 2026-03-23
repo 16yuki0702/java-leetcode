7c461571c211bcd77e752ba27bc6ce8925836dac
16yuki0702 <hs19870702@gmail.com>
2026-03-23 15:13:45 +0900

    Add cases

--- main.java
+++ main.java
@@ -7527,3 +7527,20 @@ class Solution {
         return result;
     }
 }
+
+// 331. Verify Preorder Serialization of a Binary Tree
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int diff = 1;
+        for (String node : nodes) {
+            if (--diff < 0) {
+                return false;
+            }
+            if (!node.equals("#")) {
+                diff += 2;
+            }
+        }
+        return diff == 0;
+    }
+}
