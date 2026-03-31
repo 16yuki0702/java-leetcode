7970a9a9780d124662ee9802fe68448dedc50dde
16yuki0702 <hs19870702@gmail.com>
2026-03-31 09:23:03 +0900

    Add cases

--- main.java
+++ main.java
@@ -7544,3 +7544,24 @@ class Solution {
         return diff == 0;
     }
 }
+
+// 332. Reconstruct Itinerary
+class Solution {
+    private Map<String, PriorityQueue<String>> targets = new HashMap<>();
+    private List<String> route = new LinkedList<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        for (List<String> ticket : tickets) {
+            this.targets.computeIfAbsent(ticket.get(0), k -> new PriorityQueue()).add(ticket.get(1));
+        }
+        visit("JFK");
+        return route;
+    }
+
+    private void visit(String airport) {
+        while (this.targets.containsKey(airport) && !targets.get(airport).isEmpty()) {
+            visit(targets.get(airport).poll());
+        }
+        route.add(0, airport);
+    }
+}
