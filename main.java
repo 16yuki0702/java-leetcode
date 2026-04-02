cc289b11d561be7fdd5016d949e29b9a6568f740
16yuki0702 <hs19870702@gmail.com>
2026-04-02 09:50:30 +0900

    Add cases

--- main.java
+++ main.java
@@ -7565,3 +7565,20 @@ class Solution {
         route.add(0, airport);
     }
 }
+
+// 334. Increasing Triplet Subsequence
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= min1) {
+                min1 = n;
+            } else if (n <= min2) {
+                min2 = n;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
