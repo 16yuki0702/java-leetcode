03a1796d91fa3fde60a8489e3d8c9f5ee4c0bf6c
16yuki0702 <hs19870702@gmail.com>
2026-04-03 10:09:56 +0900

    Add cases

--- main.java
+++ main.java
@@ -7582,3 +7582,34 @@ class Solution {
         return false;
     }
 }
+
+// 335. Self Crossing
+class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+        if (distance.length <= 3) {
+            return false;
+        }
+
+        int i = 2;
+        while (i < distance.length && distance[i] > distance[i - 2]) {
+            i++;
+        }
+
+        if (i >= distance.length) {
+            return false;
+        }
+        if ((i >= 4 && distance[i] >= distance[i - 2] - distance[i - 4]) ||
+            (i == 3 && distance[i] == distance[i - 2])) {
+                distance[i - 1] -= distance[i - 3];
+        }
+        i++;
+
+        while (i < distance.length) {
+            if (distance[i] >= distance[i - 2]) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
