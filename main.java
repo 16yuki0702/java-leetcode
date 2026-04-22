f0bcb3240c885d44544afb4b936875454b3d278f
16yuki0702 <hs19870702@gmail.com>
2026-04-22 14:37:22 +0900

    Add cases

--- main.java
+++ main.java
@@ -7694,3 +7694,40 @@ class Solution {
         return true;
     }
 }
+
+// 337. House Robber III
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int res[] = robHouse(root);
+        return Math.max(res[0], res[1]);
+    }
+    private int[] robHouse(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int left[] = robHouse(root.left);
+        int right[] = robHouse(root.right);
+
+        int res[] = new int[2];
+
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] + right[0];
+
+        return res;
+    }
+}
